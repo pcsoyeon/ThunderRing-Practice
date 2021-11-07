aee56d8d7b747380cc14b4b6a0138f6090e61d94
so yeon <alicesysy@swu.ac.kr>
2021-11-07 15:50:43 +0900

    [chore] 코드 수정

---
+++ ChatUI/ChatUI/ChatTVC.swift
@@ -0,0 +1,21 @@
+//
+//  ChatTVC.swift
+//  ChatUI
+//
+//  Created by soyeon on 2021/11/07.
+//
+
+import UIKit
+
+class ChatTVC: UITableViewCell {
+    static let identifier = "ChatTVC"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
--- ChatUI/ChatUI/ViewController.swift
+++ ChatUI/ChatUI/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var chatListTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        chatListTableView.delegate = self
+        chatListTableView.dataSource = self
+        
+        let nib = UINib(nibName: ListTVC.identifier, bundle: nil)
+        chatListTableView.register(nib, forCellReuseIdentifier: ListTVC.identifier)
+        
+        chatListTableView.separatorStyle = .none
     }
-
 
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 110
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let dvc = UIStoryboard(name: "Chat", bundle: nil).instantiateViewController(withIdentifier: "ChatVC") as? ChatVC else { return }
+        self.navigationController?.pushViewController(dvc, animated: true)
+    }
+}
+
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ListTVC.identifier) as? ListTVC else { return UITableViewCell() }
+        cell.selectionStyle = .none
+        return cell
+    }
+}
