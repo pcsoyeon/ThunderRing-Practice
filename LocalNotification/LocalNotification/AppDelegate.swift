2a1b78e2e9f05c240ab02e7de896f2f541be8b6a
so yeon <alicesysy@swu.ac.kr>
2021-11-17 21:40:44 +0900

    [chore] 필요 없는 코드 주석 처리

--- LocalNotification/LocalNotification/AppDelegate.swift
+++ LocalNotification/LocalNotification/AppDelegate.swift
@@ -13,7 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        UNUserNotificationCenter.current().delegate = self
+        
+        
+//        UNUserNotificationCenter.current().delegate = self
+        
+        
         return true
     }
 
@@ -32,20 +36,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 }
 
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                willPresent notification: UNNotification,
-                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.alert, .badge, .sound])
-    }
-
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                didReceive response: UNNotificationResponse,
-                                withCompletionHandler completionHandler: @escaping () -> Void) {
-
-        // deep link처리 시 아래 url값 가지고 처리
-        let url = response.notification.request.content.userInfo
-
-        completionHandler()
-    }
-}
+//extension AppDelegate: UNUserNotificationCenterDelegate {
+//    func userNotificationCenter(_ center: UNUserNotificationCenter,
+//                                willPresent notification: UNNotification,
+//                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+//        completionHandler([.alert, .badge, .sound])
+//    }
+//
+//    func userNotificationCenter(_ center: UNUserNotificationCenter,
+//                                didReceive response: UNNotificationResponse,
+//                                withCompletionHandler completionHandler: @escaping () -> Void) {
+//
+//        // deep link처리 시 아래 url값 가지고 처리
+//        let url = response.notification.request.content.userInfo
+//
+//        completionHandler()
+//    }
+//}
